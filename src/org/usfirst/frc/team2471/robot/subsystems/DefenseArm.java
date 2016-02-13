b3ee891e470fc9ea278fa71c8e3203dde05cb5b1
tylerscheuble <tylerscheuble@gmail.com>
2016-02-12 16:29:48 -0800

    Revert "Revert "configured talon ids and did some refactoring""
    This reverts commit ce56f31e7b3295efcea9babf6a45dc56a7a64cd2.

--- src/org/usfirst/frc/team2471/robot/subsystems/DefenseArm.java
+++ src/org/usfirst/frc/team2471/robot/subsystems/DefenseArm.java
@@ -13,15 +13,15 @@ public class DefenseArm extends Subsystem{
 	}
 	
 	public void rotateUp(){
-		RobotMap.rotateArm.set(1.0);
+		RobotMap.portcullisArmLeft.set(1.0);
 	}
 	
 	public void rotateDown(){
-		RobotMap.rotateArm.set(-1.0);
+		RobotMap.portcullisArmLeft.set(-1.0);
 	}
 	
 	public void rotateStop(){
-		RobotMap.rotateArm.set(0.0);
+		RobotMap.portcullisArmLeft.set(0.0);
 	}
 
 }
