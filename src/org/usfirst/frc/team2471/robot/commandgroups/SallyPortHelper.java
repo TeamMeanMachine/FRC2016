e2785dbced6a9c9971cab7a1322c8a117230b315
Shawn Hice <shawnhice@gmail.com>
2016-03-22 18:57:10 -0700

    Revisions to SallyPortHelper

--- src/org/usfirst/frc/team2471/robot/commandgroups/SallyPortHelper.java
+++ src/org/usfirst/frc/team2471/robot/commandgroups/SallyPortHelper.java
@@ -1,9 +1,11 @@
 package org.usfirst.frc.team2471.robot.commandgroups;
 
 import org.usfirst.frc.team2471.robot.OI;
+import org.usfirst.frc.team2471.robot.RobotMap;
+import org.usfirst.frc.team2471.robot.commands.DriveDistanceCommand;
 import org.usfirst.frc.team2471.robot.commands.DriveUntilButton;
-import org.usfirst.frc.team2471.robot.commands.RotateArmToAngleWait;
-
+import org.usfirst.frc.team2471.robot.commands.RotateArmToAngle;
+import org.usfirst.frc.team2471.robot.commands.RotateToAngle;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -14,9 +16,15 @@ public class SallyPortHelper extends CommandGroup {
 	public SallyPortHelper() {
 		double sallyPortAngle = SmartDashboard.getNumber("SallyPortPreset", 36.0);
 		// Grab sally port door
-		addSequential(new RotateArmToAngleWait(sallyPortAngle, 1));
-		// Have driver pull door back to safe position to flick
-		addSequential(new DriveUntilButton(OI.helperSequence));
-		// Flick door
+		addSequential(new RotateArmToAngle(sallyPortAngle));
+		// pull door back to safe position to flick
+		addSequential(new DriveDistanceCommand(1.5, 0.0, -0.5));
+		// Flick door left
+		double currentGyro = RobotMap.navx.getAngle();
+		addSequential(new RotateToAngle(currentGyro - 20, 0.5));
+		// flick back
+		addSequential(new RotateToAngle(currentGyro, 0.5));
+		// raise arm
+		addParallel(new RotateArmToAngle(sallyPortAngle+10));
 	}
 }
